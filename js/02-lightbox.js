9e2bd3a2397f68def8d80bb584584b90fe9a26c0
vladaorlik <vladaorlik@users.noreply.github.com>
2023-01-23 20:12:56 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const cardGallery = document.querySelector(".gallery");
+const cardEl = createPictureMarkup(galleryItems);
+cardGallery.insertAdjacentHTML("beforeend", cardEl);
+
+function createPictureMarkup(cardGallery) {
+    return cardGallery.map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join("");
+};
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
